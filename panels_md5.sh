2dbe9e1ad7e60fcc63a201780b114f4f9774465d
Jason3x <jason_2x@hotmail.com>
2025-09-04 12:52:11 +0200

    Update panels_md5.sh

--- panels_md5.sh
+++ panels_md5.sh
@@ -77,7 +77,7 @@ declare -Ag dtb_md5=(
 
   ["31f968d5a1d000a42c82207fb7c875b7"]="Clone R36S Panel 5"
   ["5c295b222d0ec114a4fda02d40f3411b"]="Clone R36S Panel 5"
-  ["c9eb94f10298a567efe0506ca07bbf66"]="Clone R36S Panel 5"
+  ["c9eb94f10298a567efe0506ca07bbf66"]="Clone R36S Panel 5 or 8"
   ["06674e5e8380f25da8162716333712e4"]="Clone R36S Panel 5"
   ["88f853daa7524244ca4ce7a7da3bedbc"]="Clone R36S Panel 5"
 
@@ -86,7 +86,6 @@ declare -Ag dtb_md5=(
   ["01de1c13af2882c697e013224f40bc5b"]="Clone R36S Panel 7"
 
   ["2cc8f4054fe0b5db5aac45d36c46779c"]="Clone R36S Panel 8"
-  ["c9eb94f10298a567efe0506ca07bbf66"]="Clone R36S Panel 8"
 
   ["2be6f8864414af729c0666089ac3af88"]="Clone R36S Panel 9"
 
